dcab70e50470119dd6962e1784400e0e60e1d430
DanelFaustno <daniel_sanchez.98@outlook.com>
2023-02-01 20:47:42 -0600

    INCISO 1 REALIZADO

--- practicas/p03/Manejo_de_variables_con_PHP.php
+++ practicas/p03/Manejo_de_variables_con_PHP.php
@@ -7,6 +7,30 @@
     <title>Manejo de variables con PHP</title>
 </head>
 <body>
+<h2>Inciso 1</h2>
+    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
+    <?php
+        $_myvar = "PRIMERO";
+        echo '$_myvar es correcta válido, inicia con un underscore.<br>';
 
+        $_7var = "SEGUNDO";
+        echo '$_7var es correcta válido, inicia con un underscore.<br>' ;
+
+        // myvar = "TERCERO";
+        echo 'myvar es incorrecta, no inicia con el signo de dolar.<br>';
+
+        $myvar = "CUARTO";
+        echo '$myvar es correcta válido, inicia con el signo de dolar.<br>';
+
+        $var7 = "QUINTO";
+        echo '$var7 es correcta válido, inicia con el signo de dolar.<br>';
+
+        $_element1 = "SEXTO";
+        echo '$_element1 es correcta válido, inicia con un underscorer.<br>';
+
+        // $house*5 = "SEPTIMO";
+        echo '$house*5 es incorrecta, intenta hacer una multiplicacion con la variable.<br>';
+    ?>
 </body>
 </html>

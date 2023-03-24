7950a772f9672ae969db9c8f6546e9ae856717fc
DanelFaustno <daniel_sanchez.98@outlook.com>
2023-03-23 23:38:22 -0600

    creacion de la busqueda de productos

---
+++ practicas/p08/app.js
@@ -0,0 +1,94 @@
+// JSON BASE A MOSTRAR EN FORMULARIO
+var baseJSON = {
+    "precio": 0.0,
+    "unidades": 1,
+    "modelo": "XX-000",
+    "marca": "NA",
+    "detalles": "NA",
+    "imagen": "img/default.png"
+  };
+  
+// FUNCIÓN CALLBACK DE BOTÓN "Buscar"
+function buscarID(e) {
+    e.preventDefault();
+
+    // SE OBTIENE EL ID A BUSCAR
+    var id = document.getElementById('search').value;
+
+    // SE CREA EL OBJETO DE CONEXIÓN ASÍNCRONA AL SERVIDOR
+    var client = getXMLHttpRequest();
+    client.open('POST', './backend/read.php', true);
+    client.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    client.onreadystatechange = function () {
+        // SE VERIFICA SI LA RESPUESTA ESTÁ LISTA Y FUE SATISFACTORIA
+        if (client.readyState == 4 && client.status == 200) {
+            console.log('[CLIENTE]\n'+client.responseText);
+            
+            // SE OBTIENE EL OBJETO DE DATOS A PARTIR DE UN STRING JSON
+            let productos = JSON.parse(client.responseText);    // similar a eval('('+client.responseText+')');
+            
+            // SE VERIFICA SI EL OBJETO JSON TIENE DATOS
+            if(Object.keys(productos).length > 0) {
+                // SE CREA UNA LISTA HTML CON LA DESCRIPCIÓN DEL PRODUCTO
+                let descripcion = '';
+                    descripcion += '<li>precio: '+productos.precio+'</li>';
+                    descripcion += '<li>unidades: '+productos.unidades+'</li>';
+                    descripcion += '<li>modelo: '+productos.modelo+'</li>';
+                    descripcion += '<li>marca: '+productos.marca+'</li>';
+                    descripcion += '<li>detalles: '+productos.detalles+'</li>';
+                
+                // SE CREA UNA PLANTILLA PARA CREAR LA(S) FILA(S) A INSERTAR EN EL DOCUMENTO HTML
+                let template = '';
+                    template += `
+                        <tr>
+                            <td>${productos.id}</td>
+                            <td>${productos.nombre}</td>
+                            <td><ul>${descripcion}</ul></td>
+                        </tr>
+                    `;
+
+                // SE INSERTA LA PLANTILLA EN EL ELEMENTO CON ID "productos"
+                document.getElementById("productos").innerHTML = template;
+            }
+        }
+    };
+    client.send("id="+id);
+}
+
+
+
+
+// SE CREA EL OBJETO DE CONEXIÓN COMPATIBLE CON EL NAVEGADOR
+function getXMLHttpRequest() {
+    var objetoAjax;
+
+    try{
+        objetoAjax = new XMLHttpRequest();
+    }catch(err1){
+        /**
+         * NOTA: Las siguientes formas de crear el objeto ya son obsoletas
+         *       pero se comparten por motivos historico-académicos.
+         */
+        try{
+            // IE7 y IE8
+            objetoAjax = new ActiveXObject("Msxml2.XMLHTTP");
+        }catch(err2){
+            try{
+                // IE5 y IE6
+                objetoAjax = new ActiveXObject("Microsoft.XMLHTTP");
+            }catch(err3){
+                objetoAjax = false;
+            }
+        }
+    }
+    return objetoAjax;
+}
+
+function init() {
+    /**
+     * Convierte el JSON a string para poder mostrarlo
+     * ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
+     */
+    var JsonString = JSON.stringify(baseJSON,null,2);
+    document.getElementById("description").value = JsonString;
+}
